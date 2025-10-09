5151ae4e55391cece391c4ff8b0b196794f39863
coregulab <160511685+coregulab@users.noreply.github.com>
2025-10-10 00:35:58 +0530

    Update UserRepository.java

--- authservice/src/main/java/com/authservice/repository/UserRepository.java
+++ authservice/src/main/java/com/authservice/repository/UserRepository.java
@@ -7,7 +7,7 @@ import com.authservice.entity.User;
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 	
-	User findByUsername(String username);
+	User findByUsername(String username);//same as a call second call using FeignClient method 
 	User findByEmail(String email);
 	boolean existsByUsername(String username);
 	boolean existsByEmail(String email);
